e7f261d013a108e6b2ca1ed9cd0b1180e605bb82
SharonGur27 <85168742+SharonGur27@users.noreply.github.com>
2025-12-03 09:39:03 +0200

    Add files via upload

---
+++ Escaping/Program.cs
@@ -0,0 +1,26 @@
+﻿var p = CreatePoint();
+Console.WriteLine(ComputeSum());
+
+
+static int ComputeSum()
+{
+    Point p = new Point(3, 4);
+    return p.Sum(); 
+}
+
+static Point CreatePoint()
+{
+    // Object escapes because it's returned
+    return new Point(10, 20);
+}
+
+
+class Point
+{
+    private int _x, _y;
+
+    public Point(int x, int y) { _x = x; _y = y; }
+
+    public int Sum() => _x + _y;
+}
+
