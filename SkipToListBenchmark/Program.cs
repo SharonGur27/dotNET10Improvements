3dee89fcfcd68e320b1becd095c52fa8b104dc44
SharonGur27 <85168742+SharonGur27@users.noreply.github.com>
2025-12-03 09:34:55 +0200

    Add files via upload

---
+++ SkipToListBenchmark/Program.cs
@@ -0,0 +1,15 @@
+﻿// dotnet run -c Release -f net9.0 --filter "*" --runtimes net9.0 net10.0
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+BenchmarkSwitcher.FromAssembly(typeof(SkipToListBenchmark).Assembly).Run(args);
+
+[HideColumns("Job", "Error", "StdDev", "Median", "RatioSD")]
+public partial class SkipToListBenchmark
+{
+    private readonly IEnumerable<string> _source = Enumerable.Range(0, 10000).Select(i => i.ToString()).ToArray();
+
+    [Benchmark]
+    public List<string> SkipTakeToList() => _source.Skip(2000).Take(2000).ToList();
+}
