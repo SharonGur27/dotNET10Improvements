85262dd1b49ca1c506c0cd237ef94fb8ca319c2b
SharonGur27 <85168742+SharonGur27@users.noreply.github.com>
2025-12-03 09:37:26 +0200

    Add files via upload

---
+++ JSONSerializerBenchmark/Program.cs
@@ -0,0 +1,27 @@
+﻿// dotnet run -c Release -f net9.0 --filter "*" --runtimes net9.0 net10.0
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Text.Json;
+
+BenchmarkSwitcher.FromAssembly(typeof(JSONSerializerBenchmark).Assembly).Run(args);
+
+[MemoryDiagnoser(displayGenColumns: false)]
+[HideColumns("Job", "Error", "StdDev", "Median", "RatioSD")]
+public partial class JSONSerializerBenchmark
+{
+    private Data _data = new();
+    private MemoryStream _stream = new();
+
+    [Benchmark]
+    public void Serialize()
+    {
+        _stream.Position = 0;
+        JsonSerializer.Serialize(_stream, _data);
+    }
+
+    public class Data
+    {
+        public int Value1 { get; set; }
+    }
+}
